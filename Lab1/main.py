434fec912503bcc3e74db86174eee49f8a801d2a
erjabr <erjabr2@gmail.com>
2020-08-28 14:47:44 +0200

    Solved perceptron learning

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 n = 100
-mA = [0.0, 0.5]
+mA = [-0.5, 0.0]
 mB = [-0.5, 0.0]
 #if we want to shuffle set it to 1
 shuffle = 0
@@ -31,23 +31,36 @@ def generate_matrices():
 
     input_matrix_X = []
 
-    input_matrix_X = np.array([np.concatenate([set_a1, set_a2]), np.concatenate([set_b1, set_b2]), np.array([1] * (2*n))])
+    input_matrix_X = np.array([np.concatenate((set_a1, set_a2)), np.concatenate((set_b1, set_b2)), np.array([1] * (2*n)),np.array([0] * (2*n))])
 
     #input_matrix_X = np.array([np.concatenate([set_a1, set_a2]), np.concatenate([set_b1, set_b2])])
-    weight_matrix_W = np.random.rand(1,n)
+    weight_matrix_W = np.random.rand(1,2*n)
     target_matrix_T = np.concatenate([[0.5]*n, [-0.5]*n])
     return input_matrix_X, weight_matrix_W, target_matrix_T
 
 def delta_rule(X,W,t,eta = 0.001):
     return -eta*(W@X-t)@np.transpose(X)
 
-# Variable to see if we have converged
-convergence = 0
-def delta_train(X, W, T, epoch):
-    #Iterate epoch times
-    for i in (0, epoch):
-        delta = delta_rule(X,W,T)
-        W = W + delta
+def perceptron_rule(eta,e,X):
+    return eta*(e@np.transpose(X))
+def perceptron_learning(X,W,T,epoch):
+    e = 0
+    output = []
+    for i in range(epoch):
+        y_predict = W@X
+        for i in range(n*2):
+            if y_predict[i] > 0:
+                output[i] = 1
+            else:
+                output[i] = 0
+        #Train weights using delta rule
+        e = T[i] - y_predict[i]
+        deltaW = perceptron_rule(0.001,e,X)
+        W = W + deltaW
+        print(W)
+
+
+
 
 """
 Plot the input matrix
@@ -70,8 +83,22 @@ def convergence_check(prev_val, new_val):
         return True
     return False
 
-X, W, T = generate_matrices()
-plot(X)
+W = np.array([1, 0.5 ,0.1])
+X = np.array([[-1,1,-1,1],[-1,-1,1,1],[1,1,1,1]])
+T = np.array([-1,-1,-1,1])
+e = np.array([0,0,0,0])
+for i in range(2000):
+    output = np.array([0, 0, 0, 0])
+    Y = W @ X
+    for j in range(4):
+        if Y[j] <= -1:
+            output[j] = -1
+        else:
+            output[j] = 1
+        e[j] = T[j] - output[j]
+    deltaW = perceptron_rule(0.01,e,X)
+    W = deltaW + W
+    print(output)
+    print(W)
 
 
-#delta_train(X,W,T,100)
