9ad3438fb82ebd6ded186ca2417381f6019ead1f
Bund <wilhelmagren98@gmail.com>
2020-08-29 17:15:20 +0200

    Working perceptron_learning rule. See file for comments explaining.

--- Lab1/perceptron_learning.py
+++ Lab1/perceptron_learning.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
-mA = [0.0, 1]
-mB = [-1, 0.0]
+mA = [-1.0, 1.0]
+mB = [-1.0, 1.0]
 sigmaA = 0.5
 sigmaB = 0.5
 use_bias = True
@@ -29,7 +29,9 @@ def generate_matrices():
     # X.shape[0] gives it the same dimensions as the number of rows in X
     #     |-> the dimensions are 1x3
     W = np.array([np.random.normal(0, 1, X.shape[0])])
-    W[0][0] = 1.0
+
+    # Placing the bias in the last spot of the weight matrix W yields the correct delta_W
+    W[0][2] = 1.0
 
     # Target matrix generation is correct.
     #     |-> the dimensions are 1x2n
@@ -54,11 +56,11 @@ def plot_all(X, W):
     plt.title("Perceptron learning plot")
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.ylim(top = 5, bottom = -5)
-    x = np.linspace(-2, 2, 200)
-    bias = W[0][0]
-    k = -(bias/W[0][2])/(bias/W[0][1])
-    m = -bias/W[0][2]
+    plt.ylim(top = 3, bottom = -3)
+    x = np.linspace(-3, 3, 200)
+    bias = W[0][2]
+    k = -(bias/W[0][1])/(bias/W[0][0])
+    m = -bias/W[0][1]
     y = k*x + m
     plt.plot(x, y, color="green")
     plt.show()
@@ -75,7 +77,7 @@ def perceptron_learning(X, W, T, num_epoch):
         # W has dimensions: 1x3, X has dimensions: 3x2n
         #     |-> Y-prime will get dimensions: 1x2n
         Y_prime = W@X
-        for j in range(4):
+        for j in range(2*n):
             if Y_prime[0][j] <= -1:
                 Y[0][j] = -1
             else:
