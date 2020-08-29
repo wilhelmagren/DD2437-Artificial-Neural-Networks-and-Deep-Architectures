7fdae64ae1d137606d64af7ac8067df1534a913d
Bund <wilhelmagren98@gmail.com>
2020-08-29 16:54:59 +0200

    Rewrite for perceptron learning. Almost there - something might be wrong with rule or matrix multiplications since the perceptron isn't learning correctly. See code for changes and doc."

---
+++ Lab1/perceptron_learning.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 100
+mA = [0.0, 1]
+mB = [-1, 0.0]
+sigmaA = 0.5
+sigmaB = 0.5
+use_bias = True
+
+def generate_matrices():
+    # Bias is the third row in X.
+    #    |-> Dimensions are: 3x2n
+    X = np.ones([3, 2*n])
+
+    # Randomize set_1 both x & y data
+    X[0, :n] = np.random.rand(1, n) * sigmaA + mA[0]
+    X[0, n:] = np.random.rand(1, n) * sigmaA + mA[1]
+
+    # Randomize set_2 both x & y data
+    X[1, :n] = np.random.rand(1, n) * sigmaB + mB[0]
+    X[1, n:] = np.random.rand(1, n) * sigmaB + mB[1]
+
+    # Set the bias
+    X[2, :2*n] = 1 if use_bias else 0
+
+
+    # Weight matrix generation works (Also with bias)
+    # X.shape[0] gives it the same dimensions as the number of rows in X
+    #     |-> the dimensions are 1x3
+    W = np.array([np.random.normal(0, 1, X.shape[0])])
+    W[0][0] = 1.0
+
+    # Target matrix generation is correct.
+    #     |-> the dimensions are 1x2n
+    T = np.array([np.concatenate([[-1]*n, [1]*n])])
+
+    # Plot the generated data sets found in X
+    #plot_sets(X)
+    return X, W, T
+
+def plot_sets(X):
+    plt.scatter(X[0, n:], X[1, n:], color="red")
+    plt.scatter(X[0, :n], X[1, :n], color="blue")
+    plt.title("Perceptron learning plot")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+
+def plot_all(X, W):
+    plt.scatter(X[0, n:], X[1, n:], color="red")
+    plt.scatter(X[0, :n], X[1, :n], color="blue")
+    plt.title("Perceptron learning plot")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.ylim(top = 5, bottom = -5)
+    x = np.linspace(-2, 2, 200)
+    bias = W[0][0]
+    k = -(bias/W[0][2])/(bias/W[0][1])
+    m = -bias/W[0][2]
+    y = k*x + m
+    plt.plot(x, y, color="green")
+    plt.show()
+
+
+def perceptron_rule(X, E, eta):
+    return eta*(E@np.transpose(X))
+
+
+def perceptron_learning(X, W, T, num_epoch):
+    E = np.zeros([1, 2*n])
+    Y = np.zeros([1, 2*n])
+    for i in range(num_epoch):
+        # W has dimensions: 1x3, X has dimensions: 3x2n
+        #     |-> Y-prime will get dimensions: 1x2n
+        Y_prime = W@X
+        for j in range(4):
+            if Y_prime[0][j] <= -1:
+                Y[0][j] = -1
+            else:
+                Y[0][j] = 1
+            E[0][j] = T[0][j] - Y[0][j]
+        delta_W = perceptron_rule(X, E, 0.001)
+        W = delta_W + W
+    return W
+
+
+def main():
+    input_x, weight, target = generate_matrices()
+    plot_all(input_x, weight)
+    new_weight = perceptron_learning(input_x, weight, target, 10000)
+    plot_all(input_x, new_weight)
+
+
+if __name__ == "__main__":
+    main()
