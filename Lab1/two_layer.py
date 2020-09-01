ba0ddf623de59298cf34dc73ce962abc850311ee
erjabr <erjabr2@gmail.com>
2020-09-01 22:27:59 +0200

    Added base to 2nd layer perceptron

---
+++ Lab1/two_layer.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+n = 100
+hidden_neurons = 3
+mA = [1.0,0.3]
+mB = [0.0,-0.1]
+sigmaA = 0.2
+sigmaB = 0.3
+
+def phi(x):
+    return (2 / (1 + np.exp(-x))) - 1
+def phi_prime(x):
+    return ((1 + phi(x)) * (1 - phi(x))) / 2
+
+def generate_matrices():
+    X = np.zeros([3,2*n])
+    #Make non linear sets as before, i used normal cuz it was fucked when i did random.rand and concat
+    X[0, :n] = np.concatenate((np.random.normal(1, 1, int(n / 2)) * sigmaA - mA[0], np.random.normal(1, 1, int(n / 2)) * sigmaA + mA[0]))
+    X[0,n:] = np.random.normal(0,1,n) * sigmaB + mB[0]
+    X[1,:n] = np.random.normal(0,1,n) * sigmaA + mA[1]
+    X[1,n:] = np.random.normal(0,1,n) * sigmaB + mB[1]
+    #added biased
+    X[2, :2*n] = 1.0
+    # Vikter från X -> Hidden layers
+    W = np.random.normal(1,0.5,(hidden_neurons,X.shape[0]))
+    #Vikter från Hidden layers -> Output, jag la till 1 så att vi kan hantera biased och matris multiplikation
+    V =  np.random.normal(1,0.5,(1,hidden_neurons + 1))
+
+    return X,W,V
+
+def plot(X):
+    plt.scatter(X[0,:n],X[1,:n])
+    plt.scatter(X[0,n:],X[1,n:])
+    plt.show()
+#Forward pass är att gå från X till output
+def forward_pass(X,W,V):
+    hin = W @ X
+    hout = np.concatenate((phi(hin), np.ones((1, X.shape[1]))))
+    oin = V @ hout
+    out = phi(oin)
+    return hout,out
+
+def back_pass(out,hout,T,V):
+    delta_o = out-T * phi_prime(out)
+    delta_h = (np.transpose(V)@delta_o) * phi_prime(hout)
+    #Gotta remove the last part(bias)
+    delta_h = delta_h[:hidden_neurons,:]
+    return delta_h,delta_o
+
+X,W,V = generate_matrices()
+hout, out = forward_pass(X,W,V)
+print(hout)
+print(out)
+#plot(X)
