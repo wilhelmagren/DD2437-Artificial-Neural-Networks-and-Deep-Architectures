0856bc8b77fbc3d1e75901806f5b425cea9dd28c
Bund <wilhelmagren98@gmail.com>
2020-09-08 13:32:38 +0200

    MEAN SEQUARE ARRORR AVERGAE WE GO 4 0.1

--- Lab1/part2.py
+++ Lab1/part2.py
@@ -15,7 +15,7 @@ testsamples = 200
 training = 1000
 max = 1501
 predict = 5
-hidden_neurons = 5
+hidden_neurons = 8
 eta = 0.001
 output_nodes = 1
 momen = 0.5
@@ -67,6 +67,14 @@ def split_data(input, output):
     return train_x, train_t, test_x, test_t
 
 
+def average_mse(history):
+    n = len(history)
+    sum = 0
+    for mse in history:
+        sum += mse
+    return sum/n
+
+
 def calculate_error(Y, T):
     return np.sum((T - Y[0]) ** 2)/len(Y[0])
 
@@ -77,19 +85,26 @@ def model_the_fucking_data(training_data, target_data, test_x, test_t, epoch=100
     model.add(keras.Input(shape=(5, )))
     model.add(keras.layers.Dense(hidden_neurons))
     if(drop_out):
-        model.add(keras.layers.Dropout(0.1))
+        model.add(keras.layers.Dropout(0.25))
     model.add(keras.layers.Dense(output_nodes))
     sgd = keras.optimizers.SGD(learning_rate=eta, momentum=momen)
     early_ritsch = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
     model.compile(optimizer=sgd,
                     loss='mse')
     start_t = time.time()
-    hist = model.fit(training_data, target_data, epochs=epoch, batch_size=32, verbose=1, validation_split=0.5, callbacks=[early_ritsch])
+    hist = model.fit(training_data, target_data, epochs=epoch, batch_size=32, verbose=1, validation_split=0.2, callbacks=[early_ritsch])
     end_t = time.time()
     print('time tooked: ', end_t - start_t)
     prediction_van_darkholme = model.predict(test_x)
     # i van darkholme har vi alla predictions. sen gör vi MSE med predictions och test_t DETTA GER OSS MSE för test
-    print(f"Training MSE: {hist.history['val_loss'][-1]}")
+    #
+    #
+    #
+    #
+    # TO DO:
+    # AVERGAE MSE FOR HIST-HISTORY otherwise we just check smallest. FOR PREDICTION WE DO MSE OVER ALL THINGS SO IT IS LIKE AVERAGE
+
+    print(f"Training MSE: {average_mse(hist.history['val_loss'])}")
     print(f"Testing MSE: {calculate_error(prediction_van_darkholme.T, test_t)}")
     #weight, bias = model.layers[0].get_weights()
     plt.title('Learning Curves')
