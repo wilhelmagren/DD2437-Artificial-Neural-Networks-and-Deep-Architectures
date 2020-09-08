54dac9b03ab1f2efd86eb2cf6bdc765b20f7d15d
Bund <wilhelmagren98@gmail.com>
2020-09-08 09:18:24 +0200

    splittedededededededede data

--- Lab1/part2.py
+++ Lab1/part2.py
@@ -9,7 +9,7 @@ tao = 25
 samples = 1200
 testsamples = 200
 training = 500
-valid_training = 500
+valid_training = 1000
 max = 1501
 predict = 5
 
@@ -18,6 +18,8 @@ def generate_input(x):
     input = np.array([x[t-20],x[t-15],x[t-10],x[t-5],x[t]])
     output = x[t + predict]
     return input, output
+
+
 def plot(x):
     y = np.linspace(-5, 1500, 1506)
     plt.plot(y,x)
@@ -39,25 +41,27 @@ def mackey_glass(T):
     return x
 
 
-x = mackey_glass(max + predict)
-
-input, output = generate_input(x)
-
-
 def split_data(input,output):
     #print(input.shape)
-    train_x = np.zeros([5,training])
-    test_x = np.zeros([5,testsamples])
-    train_t = np.zeros([1,training])
-    test_t = np.zeros([1,testsamples])
-    valid_x = np.zeros([5,])
+    # training 500 -> training_t
+    # validation 500 -> validation_t
+    # testing 200 -> testing_t
+
+    train_x = np.zeros([5, training])
+    test_x = np.zeros([5, testsamples])
+    train_t = np.zeros([1, training])
+    test_t = np.zeros([1, testsamples])
+    valid_x = np.zeros([5, training])
+    valid_t = np.zeros([1, training])
     for rows in range(len(train_x)):
         train_x[rows] = input[rows][:training]
-        test_x[rows] = input[rows][training:]
+        test_x[rows] = input[rows][valid_training:]
+        valid_x[rows] = input[rows][training:valid_training]
 
     train_t = output[:training]
-    test_t = output[training:]
-
+    valid_t = output[training:valid_training]
+    test_t = output[valid_training:]
+    return train_x, train_t, valid_x, valid_t, test_x, test_t   
 
 
 x = mackey_glass(max + predict)
