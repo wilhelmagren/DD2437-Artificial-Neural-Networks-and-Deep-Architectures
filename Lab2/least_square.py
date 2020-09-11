970a9ef34c5be30e5149aba5ad8ff5e0bf832519
Bund <wilhelmagren98@gmail.com>
2020-09-11 13:38:08 +0200

    init commit

---
+++ Lab2/least_square.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Lab 2 lets go bismillah
+"""
+
+def perform_task():
+    print("### --- Doing the thing --- ###")
+
+def main():
+    perform_task()
+
+if __name__=="__main__":
+    main()
