4c70aa2573671ec5caabdcf68835921abd82c09f
Bund <wilhelmagren98@gmail.com>
2020-09-14 11:42:08 +0200

    seq delta

--- Lab2/least_square.py
+++ Lab2/least_square.py
@@ -54,7 +54,7 @@ import matplotlib.pyplot as plt
 N = 63  # Number of inputs
 n = 25  # Number of RBF's, has to be greater than N
 step_size = 0.1  # Used for generating sin wave
-sigma = 0.8  # Variance for all nodes
+sigma = 0.5  # Variance for all nodes
 
 
 def generate_input(use_noise=True):
@@ -70,8 +70,8 @@ def generate_input(use_noise=True):
     square_training_target = np.sign(training_target_sin)
     square_testing_target = np.sign(testing_target_sin)
     if use_noise:
-        x_training += np.random.normal(0, 0.1, (x_training.shape))
-        x_testing += np.random.normal(0, 0.1, (x_testing.shape))
+        x_training += np.random.normal(0, 0.1, x_training.shape)
+        x_testing += np.random.normal(0, 0.1, x_testing.shape)
     for i in range(len(square_training_target)):
         if square_training_target[i] == 0:
             square_training_target[i] = 1
@@ -79,40 +79,61 @@ def generate_input(use_noise=True):
         if square_testing_target[i] == 0:
             square_testing_target[i] = 1
 
-
     return x_training, x_testing, training_target_sin, testing_target_sin, square_training_target, square_testing_target
 
 
 def phi_func(x, my):
     return np.exp(-(x - my)**2 / (2*(sigma ** 2)))
+
+
 def calculate_error(target,estimate):
     return 1/2*((target-estimate)**2)
-def delta_learning_rule(error,phi,k,eta):
-    print(phi[k])
+
+
+def delta_learning_rule(error, phi, k, eta):
     return eta*error*phi[k]
+
+
 def delta_rule(square):
     train_x, test_x, sin_train_t, sin_test_t, square_train_t, square_test_t = generate_input()
     w = generate_weight()
     rbf_pos = generate_initial_rbf_position()
     phi_test = generate_big_phi(test_x, rbf_pos)
     phi_train = generate_big_phi(train_x, rbf_pos)
-    epochs = 10000
+    epochs = 1000
     train_size = len(train_x)
-    k = 0
+    error = 0
+    estimation = []
     for i in range(epochs):
+        print(f" Epoch number: [{i}]")
         if not square:
-            e = sin_train_t[k] - phi_train[k]@w
-            k = (k+1) % train_size
-            delta_w = delta_learning_rule(e,phi_test,k,0.001)
-            w += delta_w
-            #print(w)
+            for k in range(train_size):
+                # print(f" k number: [{k}]")
+                e = sin_train_t[k] - phi_train[k]@w
+                # k = (k+1) % train_size
+                delta_w = delta_learning_rule(e, phi_test, k, 0.001)
+                w += delta_w
         else:
-            e = square_train_t[k] - phi_train[k] @ w
-            k = (k + 1) % train_size
-            delta_w = delta_learning_rule(e, phi_test, k,0.001)
-            w += delta_w
-
-    if not square:
+            for k in range(train_size):
+                # print(f" k number: [{k}]")
+                e = square_train_t[k] - phi_train[k] @ w
+                # k = (k + 1) % train_size
+                delta_w = delta_learning_rule(e, phi_test, k, 0.001)
+                w += delta_w
+        if not square:
+            estimation = phi_test @ w
+            error = np.abs(estimation - sin_test_t).mean()
+            print(f"Epoch number [{i}] has error: [{error}]")
+        else:
+            estimation = phi_test @ w
+            for j in range(len(estimation)):
+                if estimation[j] >= 0:
+                    estimation[j] = 1
+                else:
+                    estimation[j] = -1
+            error = np.abs(estimation - square_test_t).mean()
+            print(f"Epoch number [{i}] has error: [{error}]")
+    """if not square:
         estimate = phi_test @ w
         error = np.abs(estimate-sin_test_t).mean()
     else:
@@ -122,11 +143,8 @@ def delta_rule(square):
                 estimation[i] = 1
             else:
                 estimation[i] = -1
-        error = np.abs(estimation-square_test_t).mean()
-    return error
-
-
-
+        error = np.abs(estimation-square_test_t).mean()"""
+    return sin_test_t, estimation
 
 
 def generate_big_phi(input_matrix, rbf_pos):
@@ -216,8 +234,6 @@ def perform_least_squared(squared=False):
     """
     Func perform_least_squared/0
         Change code to work for 'box'-function instead of sinus.
-
-        TODO: debunk why the RBF position is good for the domain [0, pi/2] and not for the rest... also why 0.06 convergence?
     """
     print("### --- Doing the least squared --- ###")
     train_x, test_x, sin_train_t, sin_test_t, square_train_t, square_test_t = generate_input()
@@ -278,12 +294,12 @@ def plot_rbf_pos(rbf):
 
 
 def main():
-    error = delta_rule(False)
-    print(error)
-    #est, tar, error_lists, it_list, rbf_positions = perform_least_squared(True)
-    #plot_error(error_lists, it_list)
+    target, est = delta_rule(False)
+    plot_approximation(est, target)
+    # est, tar, error_lists, it_list, rbf_positions = perform_least_squared(True)
+    # plot_error(error_lists, it_list)
     # plot_rbf_pos(rbf_positions)
-    #plot_approximation(est, tar)
+    # plot_approximation(est, tar)
 
 
 if __name__ == "__main__":
