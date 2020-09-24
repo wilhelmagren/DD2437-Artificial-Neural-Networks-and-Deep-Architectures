830b8a98419a8297a1a5ce3b85ada90fbaa3bffc
Bund <wilhelmagren98@gmail.com>
2020-09-24 14:39:56 +0200

    300 100 tesitng

--- Lab3/hopfield.py
+++ Lab3/hopfield.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 # P is the number of patterns, we start with 3?
 P = 11
 # N is the number of units, what the fuck does that mean. Maybe number of neurons? Haven't implemented any neurons doe..
-N = 1024
+N = 100
 # T_P is the number of patterns we train on
-T_P = 100
+T_P = 300
 
 
 def accuracy(target, pattern):
@@ -127,7 +127,7 @@ def test_hopfield(w, dist_p, normal_p):
     return acc_list
 
 
-def calculate_weight_matrix(w, pattern_list, scale=True):
+def calculate_weight_matrix(w, pattern_list, num_train, scale=True):
     """
     @spec calculate_weight_matrix(np.array(), np.array()) :: np.array()
         Returns a modified weight matrix, where all weights have been calculated given formula below.
@@ -137,7 +137,7 @@ def calculate_weight_matrix(w, pattern_list, scale=True):
 
     for i in range(N):
         for j in range(N):
-            for mu in range(T_P):
+            for mu in range(num_train):
                 w[i][j] += pattern_list[mu][i]*pattern_list[mu][j]
             if scale:
                 w[i][j] /= N
@@ -181,8 +181,9 @@ def plot_acc(acc, it, lbl):
     plt.plot(it, acc, label=lbl)
     plt.grid()
     plt.legend()
-    plt.ylabel("Accuracy")
-    plt.xlabel("% amount of noise")
+    plt.ylabel("Number of stable points")
+    plt.xlabel("Number of patterns learned")
+    plt.show()
 
 
 def flip_data(data, i):
@@ -190,6 +191,35 @@ def flip_data(data, i):
         idx = np.random.choice(N, i*10, replace=False)
         data[row][idx] *= -1
     return data
+
+
+def modified_test_hopfield(w, pattern_list):
+    """
+    @spec test_hopfield(np.array(), np.array()) :: void
+        Iteratively try and recall the original pattern given a distorted pattern.
+        This recalled pattern is called a 'Fixed point' if we apply the update_rule and get the same pattern back.
+    """
+    num_good = 0
+    for p in pattern_list:
+        naty = p
+        recalled_pattern = hopfield_recall(w, p)
+        if not np.array_equal(recalled_pattern, p):
+            p = recalled_pattern
+            recalled_pattern = hopfield_recall(w, p)
+        if accuracy(naty, recalled_pattern) > 0.99:
+            num_good += 1
+    return num_good
+
+
+def recalculate_stable_points(patterns):
+    w = generate_weight_matrix()
+    num_good_list = []
+    for i in range(T_P):
+        print(f"Number of patterns trained: {i}")
+        w = calculate_weight_matrix(w, patterns, i)
+        # nu ska vi kolla hur många patterns som är stable
+        num_good_list.append(modified_test_hopfield(w, patterns[:i]))
+    plot_acc(num_good_list, [i for i in range(len(num_good_list))], "stable patterns")
 
 
 def main():
@@ -221,14 +251,14 @@ def main():
 
     Maybe look at this for some theory: https://www.youtube.com/watch?v=nv6oFDp6rNQ
     """
-    data = read_file()
+    # data = read_file()
     # pure_patterns, dist_patterns = generate_input_patterns()
-    #mod_data = data[0:4, :N]
-    mod_data = np.vstack((data[:3, :N], data[7, :N]))
+    # mod_data = data[0:4, :N]
+    # mod_data = np.vstack((data[:3, :N], data[7, :N]))
 
-    super_acc_list = []
-    average = 0
-    flipper = 10
+    # super_acc_list = []
+    # average = 0
+    # flipper = 10
 
     random_patterns = np.random.randint(-1,1,(T_P,N))
 
@@ -238,12 +268,15 @@ def main():
                 random_patterns[i][j] = 1
             else:
                 random_patterns[i][j] = -1
-    w = calculate_weight_matrix(generate_weight_matrix(), random_patterns)
-    temp_data = flip_data(random_patterns.copy(), flipper)
-    print(random_patterns.shape)
-    for i in range(len(random_patterns)):
-        acc_list = test_hopfield(w, temp_data[i],random_patterns[i])
-        print(acc_list)
+
+    recalculate_stable_points(random_patterns)
+
+    # w = calculate_weight_matrix(generate_weight_matrix(), random_patterns)
+    # temp_data = flip_data(random_patterns.copy(), flipper)
+    # print(random_patterns.shape)
+    # for i in range(len(random_patterns)):
+    #    acc_list = test_hopfield(w, temp_data[i],random_patterns[i])
+    #    print(acc_list)
 
 
 
